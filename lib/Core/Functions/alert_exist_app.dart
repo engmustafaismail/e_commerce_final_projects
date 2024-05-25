68adee95db82f2172a15d9413b30fc2eeb9d28e9
Eng.Mustafa_Ismail <mutfaismail123@gmail.com>
2024-05-25 08:20:29 +0300

    modify on back dialog style

--- lib/Core/Functions/alert_exist_app.dart
+++ lib/Core/Functions/alert_exist_app.dart
@@ -1,37 +1,58 @@
 import 'dart:io';
 
+import 'package:e_commerce_final_projects/Core/Constant/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AlertExit {
-  static  alerExit(BuildContext context) {
+  static alerExit(BuildContext context) {
     return Get.defaultDialog(
       title: "warningtitle".tr,
       titleStyle: Theme.of(context).textTheme.titleMedium,
       middleText: "alerexistqusetion".tr,
       middleTextStyle: Theme.of(context).textTheme.bodySmall,
       actions: [
-        TextButton(
-          onPressed: () {
-            exit(0);
-          },
-          child: Text(
-            "yes".tr,
-          ),
-        ),
-        TextButton(
+        CustomButtonDialog(
+          text: "no".tr,
           onPressed: () {
             Get.back();
           },
-          child: Text(
-            "no".tr,
-          ),
         ),
+        CustomButtonDialog(
+            text: "yes".tr,
+            onPressed: () {
+              exit(0);
+            })
       ],
     );
   }
+}
 
+class CustomButtonDialog extends StatelessWidget {
+  final String text;
+  final void Function()? onPressed;
+  const CustomButtonDialog({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: _buildTextButtonStyle(),
+      ),
+    );
+  }
 
-  
+  static TextStyle _buildTextButtonStyle() {
+    return const TextStyle(
+      color: AppColors.primary,
+      fontSize: 20.5,
+      fontWeight: FontWeight.bold,
+    );
+  }
 }
